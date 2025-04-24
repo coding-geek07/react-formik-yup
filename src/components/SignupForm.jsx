e026b9abfb70722f8f3dee0e296fa5fb88cdd9ba
coding-geek07 <exploringtechiegeek@gmail.com>
2025-04-24 11:38:21 +0530

    React Formik: Install Yup package and Add Yup validation replacing error validation of our own

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,28 +1,29 @@
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
-const validate = (values) => {
-  const errors = {};
+// const validate = (values) => {
+//   const errors = {};
 
-  if (!values.firstName) {
-    errors.firstName = "Required";
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Must be 15 characters or less";
-  }
+//   if (!values.firstName) {
+//     errors.firstName = "Required";
+//   } else if (values.firstName.length > 15) {
+//     errors.firstName = "Must be 15 characters or less";
+//   }
 
-  if (!values.lastName) {
-    errors.lastName = "Required";
-  } else if (values.lastName.length > 20) {
-    errors.lastName = "Must be 20 characters or less";
-  }
+//   if (!values.lastName) {
+//     errors.lastName = "Required";
+//   } else if (values.lastName.length > 20) {
+//     errors.lastName = "Must be 20 characters or less";
+//   }
 
-  if (!values.email) {
-    errors.email = "Required";
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = "Invalid email address";
-  }
+//   if (!values.email) {
+//     errors.email = "Required";
+//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//     errors.email = "Invalid email address";
+//   }
 
-  return errors;
-};
+//   return errors;
+// };
 
 const SignupForm = () => {
   const formik = useFormik({
@@ -31,51 +32,70 @@ const SignupForm = () => {
       lastName: "",
       email: "",
     },
-    validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .required("Required!!")
+        .max(10, "Name Should be max 15 characters!!"),
+      lastName: Yup.string()
+        .required("Required!!")
+        .max(10, "Name Should be max 10 characters!!"),
+      email: Yup.string().email("Invalid email!!").required("Email Required"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
+  const errorStyles = { color: "red" };
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
-      />
-      {formik.touched.firstName && formik.errors.firstName ? (
-        <div>{formik.errors.firstName}</div>
-      ) : null}
-
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
-      />
-      {formik.touched.lastName && formik.errors.lastName ? (
-        <div>{formik.errors.lastName}</div>
-      ) : null}
-
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <div>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.firstName}
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <div style={errorStyles}>{formik.errors.firstName}</div>
+          ) : null}
+        </div>
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <div>
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.lastName}
+          />
+          {formik.touched.lastName && formik.errors.lastName ? (
+            <div style={errorStyles}>{formik.errors.lastName}</div>
+          ) : null}
+        </div>
+      </div>
+      <div>
+        <label htmlFor="email">Email Address</label>
+        <div>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div style={errorStyles}>{formik.errors.email}</div>
+          ) : null}
+        </div>
+      </div>
 
       <button type="submit">Submit</button>
     </form>
